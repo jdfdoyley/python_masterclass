470c597c90fc98b9094f0afd8c832ee081d997f0
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-10 08:29:39 -0400

    More on escape character and raw string

--- strings/escapechar.py
+++ strings/escapechar.py
@@ -8,7 +8,8 @@ print(tabbed_string)
 
 print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
 print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
-print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
+print("""The pet shop owner said "No, no, \
+'e's uh,...he's resting".""")
 
 another_split_string = """This string has been
 split over
@@ -21,3 +22,6 @@ split over \
 several \
 lines"""
 print(another_split_string_2)
+
+print("C:\\Users\\tommy\\notes.txt")
+print(r"C:\Users\tommy\notes.txt")
