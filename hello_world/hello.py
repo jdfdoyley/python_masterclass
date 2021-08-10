a4aa7786a87da23414ea4eb52171911453722e04
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-10 07:51:05 -0400

    Python print method

---
+++ hello_world/hello.py
@@ -0,0 +1,8 @@
+"""This file is responsible for demonstrating the Python print method"""
+
+print("Hello, World!")
+print('Hello again world!')
+print(1 + 2)
+print(7 * 6)
+print()
+print("The end", "or is it?", "keep watching to learn more about Python", 3)
