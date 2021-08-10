b4b5a24e477b1dc224c12c20c8228360273adf3f
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-10 08:37:58 -0400

    Variables and the built-in type method

--- strings/strings.py
+++ strings/strings.py
@@ -6,7 +6,17 @@ print("Python's strings are easy to use")
 print('We can even include "quotes" in strings')
 print("Hello" + " Jason!")
 
-GREETING = "Hello"
-# name = "Dave"
-name = input("Please enter your name: ")
-print(GREETING + " " + name)
+greeting = "Hello"
+name = "Dave"
+# name = input("Please enter your name: ")
+print(greeting + " " + name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age = "2 years"
+print(age)
+print(type(age))
