63648ffb9ed6377a55ca7b4f614094aad703025b
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-10 07:59:23 -0400

    Intro to python strings

---
+++ strings/strings.py
@@ -0,0 +1,12 @@
+"""This file demonstrate how to use strings in Python"""
+
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("Hello" + " Jason!")
+
+GREETING = "Hello"
+# name = "Dave"
+name = input("Please enter your name: ")
+print(GREETING + " " + name)
