6afa88a7ba8d42790f71b2eebcc43bb2af9f5e67
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-11 13:16:29 -0400

    Use of valid Python operators

---
+++ hello_world/operators.py
@@ -0,0 +1,17 @@
+"""This file demonstrate the use of valid Python operators."""
+
+num_1 = 12
+num_2 = 3
+
+# Operator Demo
+print(num_1 + num_2)    # 15
+print(num_1 - num_2)    # 9
+print(num_1 * num_2)    # 36
+print(num_1 / num_2)    # 4.0
+print(num_1 // num_2)   # 4 => integer division: round to minu infinity
+print(num_1 % num_2)    # 0 => modulo: returns the remainder from a division
+
+print()
+
+for i in range(1, 4):
+    print(i)
