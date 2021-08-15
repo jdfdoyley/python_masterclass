5ddcbb4fe6e8b473ae687f9d139e83a892b751d6
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-15 07:19:53 -0400

    Create a list

---
+++ python_list/list.py
@@ -0,0 +1,22 @@
+"""This file is responsible for demonstrating the use of List."""
+
+# Create a List
+# There are several ways to create a new list; the simplest is to enclose the
+# values in square brackets []
+
+# A list of integers
+int_list = [1, 2, 3]
+
+# A list of strings
+ants = ['carpenter', 'fire', 'pharaoh', 'tapinoma']
+
+# The items of a list don't have to be the same type. The following list
+# contains an integer, a string, a float, a complex number, and a boolean
+
+# A list of mixed datatypes
+mixed_list = [1, 'abc', 1.23, (3+4j), True]
+
+# A list containing zero items is called an empty list and you can create one with empty brackets []
+
+# An empty list
+empty_list = []
