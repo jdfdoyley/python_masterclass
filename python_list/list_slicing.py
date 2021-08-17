e310f5d39b4c5be315f7b0bf785d26175b33a36f
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-17 16:55:15 -0400

    Slice with positive and negative indices

--- python_list/list_slicing.py
+++ python_list/list_slicing.py
@@ -21,3 +21,10 @@ print()
 # You can also specify negative indices while slicing a list.
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(numbers[-7:-3])   # [3, 4, 5, 6]
+
+print()
+
+# SLICE WITH POSITIVE & NEGATIVE INDICES
+# You can specify both positive and negative indices at the same time.
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(numbers[-7:7])    # [3, 4, 5, 6]
