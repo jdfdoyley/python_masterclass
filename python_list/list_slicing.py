af47a5b6c0b6deb4ae22c6d6198e6e2294a1470c
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-17 16:42:29 -0400

    Slice with negative indices

--- python_list/list_slicing.py
+++ python_list/list_slicing.py
@@ -14,3 +14,10 @@
 # Basic Example
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(numbers[3:7])     # [3, 4, 5, 6]
+
+print()
+
+# SLICE WITH NEGATIVE INDICES
+# You can also specify negative indices while slicing a list.
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(numbers[-7:-3])   # [3, 4, 5, 6]
