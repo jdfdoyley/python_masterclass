1c99095e7778ebebc9a7551e8ef01da11268c277
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-17 09:51:28 -0400

    Create a nested list

---
+++ python_list/nested_list.py
@@ -0,0 +1,11 @@
+"""In this file I will demonstrate Nested List in Python."""
+
+# A nexted list is when one list contains a sublist which can also contains
+# another sublist.
+
+# You can use them to arrange data in a hirarchical structure.
+
+# CREATE A NESTED LIST
+# A nexted list is created by placing a comma-separated sequence of sublists.
+nested_list = ['a', ['bb', ['ccc', 'ddd'], 'ee', 'ff'], 'g', 'h']
+print(nested_list)
