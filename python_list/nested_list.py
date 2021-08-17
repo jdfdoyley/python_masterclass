406f93356d31dcb33f1dadad3d907e0a78bf053e
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-17 10:02:11 -0400

    Access nested list items by index

--- python_list/nested_list.py
+++ python_list/nested_list.py
@@ -9,3 +9,12 @@
 # A nexted list is created by placing a comma-separated sequence of sublists.
 nested_list = ['a', ['bb', ['ccc', 'ddd'], 'ee', 'ff'], 'g', 'h']
 print(nested_list)
+
+print()
+
+# ACCESS NESTED LIST ITEMS BY INDEX
+# You can access individual items in a nested list using multiple indexes.
+nested_list = ['a', 'b', ['cc', 'dd', ['eee', 'fff']], 'g', 'h']
+print(nested_list[2])
+print(nested_list[2][2])
+print(nested_list[2][2][0])
