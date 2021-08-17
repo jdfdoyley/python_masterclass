d0f53445bb6874d4e79e743a85656550f08eaca6
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-17 10:06:11 -0400

    Negative list indexing in a nested list

--- python_list/nested_list.py
+++ python_list/nested_list.py
@@ -18,3 +18,15 @@ nested_list = ['a', 'b', ['cc', 'dd', ['eee', 'fff']], 'g', 'h']
 print(nested_list[2])
 print(nested_list[2][2])
 print(nested_list[2][2][0])
+
+print()
+
+# NEGATIVE LIST INDEXING IN A NESTED LIST
+# You can access a nested list by negative indexing as well.
+# Negative indexes count backward from the end of the list. So, L[-1] refers to
+# the last item, L[-2] is the second-last, and so on.
+nested_list = ['a', 'b', ['cc', 'dd', ['eee', 'fff']], 'g', 'h']
+
+print(nested_list[-3])
+print(nested_list[-3][-1])
+print(nested_list[-3][-1][-2])
