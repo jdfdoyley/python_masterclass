c4b2f9d96e6d4957ec71497416dcdc7e726812d5
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-19 06:47:40 -0400

    While loop syntax

--- flow_control/while_loop.py
+++ flow_control/while_loop.py
@@ -1,1 +1,16 @@
 """In this file, I will demonstrate the use of Python while Loops."""
+
+# PYTHON WHILE LOOP
+# A while loop is used when you want to perform a task indefinitely, until a
+# particular condition is met. It’s a condition-controlled loop.
+
+# SYNTAX
+
+# while condition:
+#     statement
+#     statement
+#     ...
+# else:
+#     statement
+#     statement
+#     ...
