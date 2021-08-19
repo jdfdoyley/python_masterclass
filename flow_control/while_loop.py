5b7a4cafdff0662e85fa0fa6ef8db149127f0fc7
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-19 06:59:44 -0400

    Basic examples of while loop

--- flow_control/while_loop.py
+++ flow_control/while_loop.py
@@ -14,3 +14,39 @@
 #     statement
 #     statement
 #     ...
+
+# BASIC EXAMPLES
+# Any non-zero value or nonempty container is considered TRUE; whereas Zero,
+# None, and empty container is considered FALSE.
+
+# Iterate until x becomes 0
+x = 6
+while x:
+    print(x)
+    x -= 1
+
+print()
+
+# Iterate until list is empty
+sharks = ["hammerhead", "sand", "sawshark", "catshark", "cow"]
+while sharks:
+    print(sharks.pop())
+
+print()
+
+# Iterate until string is empty
+shark = "hammerhead"
+while shark:
+    print(shark)
+    shark = shark[1:]
+
+print()
+
+# If the condition is false at the start, the while loop will never be executed
+# at all.
+
+# Exit condition is false at the start
+x = 0
+while x:
+    print(x)
+    x -= 1
