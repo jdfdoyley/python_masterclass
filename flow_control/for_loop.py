9046672d9c3567380130d3fcaa04533c6406efee
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-20 08:37:19 -0400

    Add new file for for-loop demo

--- flow_control/for_loop.py
+++ flow_control/for_loop.py
@@ -1,2 +1,20 @@
 # pylint: disable=C0103
 """In this file I will demonstrate the use of Python for Loop."""
+
+# PYTHON FOR LOOP
+# The for statement in Python is a bit different from what you usually use in
+# other programming languages.
+
+# Rather than iterating over a number progression, Python's for statement
+# iterates over the items of any iterable (list, tuple, dictionary, set, or
+# string). The items are iterated in the order that they appear in the iterable.
+
+# SYNTAX
+# for var in iterable:
+#     statement
+#     statement
+#     ...
+# else:
+#     statement
+#     statement
+#     ...
