855096431ed301ef2b2b71eceb5cb8c9313d05fe
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-20 09:46:29 -0400

    Using the range() function in for loops

--- flow_control/for_loop.py
+++ flow_control/for_loop.py
@@ -72,6 +72,8 @@ for dog in dogs:
 else:
     print('All items have been printed!')
 
+print()
+
 # If the loop gets terminated prematurely with break, the else cluase will be
 # skipped.
 dogs = ['german shepherd', 'poodle',
@@ -84,3 +86,50 @@ else:
     print('All items have been printed!')
 
 print('I appear after the else block!')
+
+print()
+
+# RANGE() FUNCTION IN FOR LOOP
+# If you need to execute a group of statements for specified number of times,
+# use built-in function range()
+
+# The range(start, stop, step) function generates a sequence of numbers from 0
+# up to (but not including) the specified number
+
+# Generate a sequence of numbers from 0 - 6
+for num in range(7):
+    print(num)
+
+# range() provides a simple way to repeat an action a specific number of times.
+
+print()
+
+# Print 'Hello!' three times
+for x in range(3):
+    print('Hello!')
+
+print()
+
+# The range starts from 0 by default. But, you can start the range at another
+# number by specifying the start parameter.
+
+# Generate a sequence of numbers from 2 to 6
+for x in range(2, 7):
+    print(x)
+
+print()
+
+# You can generate a range of negative numbers as well.
+for x in range(-5, 0):
+    print(x)
+
+print()
+
+# The range increments by 1 by default. But, you can specify a different
+# increment by adding a step parameter.
+
+# Incrememnt the range with 2
+for x in range(2, 7, 2):
+    print(x)
+
+print()
