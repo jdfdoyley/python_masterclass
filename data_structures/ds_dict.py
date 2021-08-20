88cb4b20b9f74a2a5dd8f92a5f008e9ec3a8916f
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-20 17:33:56 -0400

    The dict() constructor

--- data_structures/ds_dict.py
+++ data_structures/ds_dict.py
@@ -16,3 +16,43 @@ emp = {
 }
 
 print(emp)
+
+print()
+
+# THE dict() CONSTRUCTOR
+# You can convert two-value sequences into a dictionary with Python's dict()
+# constructor. The first item in each sequence is used as the key and the
+# second as the value.
+
+# Create a dictionary with a lit of two-item tuples
+list_tuple = [
+    ('name', 'jason'),
+    ('age', 31),
+    ('job', 'software engineer')
+]
+
+emp = dict(list_tuple)
+print(emp)
+print()
+
+# Create a dictionary with a tuple of two-item lists
+tuple_list = (
+    ['name', 'jason'],
+    ['age', 31],
+    ['job', 'software engineer']
+)
+
+emp = dict(tuple_list)
+print(emp)
+print()
+
+# When the keys are simple strings, it is sometimes easier to specify key:value
+# pairs using keyword arguments.
+emp = dict(
+    name='jason',
+    age=25,
+    job='Dev'
+)
+
+print(emp)
+print()
