020f486fd28e26a19774b8204154cb5892caba4d
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-20 17:16:36 -0400

    Create a dictionary using curly braces

---
+++ data_structures/ds_dict.py
@@ -0,0 +1,18 @@
+# pylint: disable=C0103
+"""In this file I demonstrate Python Dictionary."""
+
+# CREATE A DICTIONARY
+# You can create a dictionary by placing a comma-separated list of key:value
+# pairs in curly braces {}. Each key is separated from its associated value by
+# a colon :
+
+# Create a dictionary to store employee records
+emp = {
+    'name': 'jason',
+    'age': 31,
+    'job': 'front office manager',
+    'city': 'norfolk',
+    'email': 'jason@email.com'
+}
+
+print(emp)
