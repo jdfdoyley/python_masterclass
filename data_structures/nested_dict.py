df1339297fd90e679e740c93c2a326276718ffef
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-23 15:36:33 -0400

    Understand the dict() constructor

--- data_structures/nested_dict.py
+++ data_structures/nested_dict.py
@@ -13,8 +13,44 @@
 # The only difference is that each value is another dictionary.
 
 # Let’s build a dictionary that stores employee record.
-employee = {
+employees1 = {
     'emp1': {'name': 'Bob', 'job': 'Mgr'},
     'emp2': {'name': 'Kim', 'job': 'Dev'},
     'emp3': {'name': 'Sam', 'job': 'Dev'}
 }
+
+# THE dict() CONSTRUCTOR
+# There are several ways to create a nested dictionary using a type constructor
+# called dict().
+
+# To create a nested dictionary, simply pass dictionary key:value pair as
+# keyword arguments to dict() Constructor.
+employees2 = dict(
+    emp1={'name': 'Bob', 'job': 'Mgr'},
+    emp2={'name': 'Kim', 'job': 'Dev'},
+    emp3={'name': 'Sam', 'job': 'Dev'}
+)
+print(employees2)
+print()
+
+# You can use dict() function along with the zip() function, to combine
+# separate lists of keys and values obtained dynamically at runtime.
+ids = ['emp1', 'emp2', 'emp3']
+emp_info = [
+    {'name': 'Bob', 'job': 'Mgr'},
+    {'name': 'Kim', 'job': 'Dev'},
+    {'name': 'Sam', 'job': 'Dev'},
+]
+
+employees3 = dict(zip(ids, emp_info))
+print(employees3)
+print()
+
+# You’ll often want to create a dictionary with default values for each key.
+# The fromkeys() method offers a way to do this.
+ids = ['emp1', 'emp2', 'emp3']
+default_values = {'name': '', 'job': ''}
+
+employees4 = dict.fromkeys(ids, default_values)
+print(employees4)
+print()
