956b82a52210bc39971e6d832ca89c1c916f6360
Jason D'Oyley <jdfdoyley@gmail.com>
2021-08-24 17:48:22 -0400

    Create tuples in Python

---
+++ data_structures/ds_tuple.py
@@ -0,0 +1,18 @@
+"""This is about Python Tuple"""
+
+# CREATE A TUPLE
+# You can create a tuple by placing a comma-separated sequence of items in
+# parentheses ().
+
+# A Tuple of integers
+integers = (1, 2, 3, 4, 5, 6)
+
+# A tuple of strings
+sharks = ("Hammerhead", "Sand", "Sawshark", "Ccatshark", "Cow")
+
+# The items of a tuple don’t have to be the same type.
+mixed = (1, 3.14, True, "Harry")
+
+# A tuple containing zero items is called an empty tuple and you can create one
+# with empty brackets ()
+empty = ()
