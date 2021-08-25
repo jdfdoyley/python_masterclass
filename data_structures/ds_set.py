09fbad868b188c6061b6771e6cbbd337834048a9
Jason D'Oyley <jdfd.dev@icloud.com>
2021-08-25 11:04:19 -0400

    Add tdefinition for Python Set

---
+++ data_structures/ds_set.py
@@ -0,0 +1,15 @@
+"""This file demonstrate the use of Python Set."""
+
+# PYTHON SET
+# Python set is an unordered collection of unique items. They are commonly used
+# for computing mathematical operations such as union, intersection,
+# difference, and symmetric difference.
+
+# The important properties of Python sets are as follows:
+
+# Sets are unordered – Items stored in a set aren’t kept in any particular
+# order.
+# Set items are unique – Duplicate items are not allowed.
+# Sets are unindexed – You cannot access set items by referring to an index.
+# Sets are changeable (mutable) – They can be changed in place, can grow and
+# shrink on demand.
